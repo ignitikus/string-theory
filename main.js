f90116c0a77a405525c426293c59e83404fe5541
NikolayK <nikolay.kim@codeimmersive.com>
2019-10-21 19:48:52 -0400

    refactors the code

--- main.js
+++ main.js
@@ -1,78 +1,70 @@
 function crazyCase(str) {
   let newNewStr = '';
-  let newStr = str.toLowerCase()
-  for (let i = 0; i<newStr.length; i++){
+  for (let i = 0; i<str.length; i++){
     if (i%2===0){
-    newNewStr = newNewStr + newStr[i];
+    newNewStr += str[i].toLowerCase();
     } else{
-      let newLetter = newStr[i];
-      let newNewLetter = newLetter.toUpperCase();
-      newNewStr = newNewStr + newNewLetter;
+      let newNewLetter = str[i].toUpperCase();
+      newNewStr += newNewLetter;
     }
   } return newNewStr;
 }
 
 function ciEmailify(str) {
-  let newNewStr = '';
-  let newStr = str.toLowerCase()
+  let emailStr = '';
   for (let i=0; i<str.length; i++){
-    if (newStr[i] !== ' '){
-      newNewStr = newNewStr + newStr[i];
+    if (str[i] !== ' '){
+      emailStr += str[i].toLowerCase();
     } else {
-      newNewStr = newNewStr + '.'
+      emailStr += '.';
     }
-  } return newNewStr +'@codeimmersives.com'
+  } return emailStr +'@codeimmersives.com'
 }
 
 function exclaim(str) {
   let newStr = '';
   for (let i=0; i<str.length; i++){
     if (str[i] === '?' || str[i] === '.'){
-      newStr = newStr + '!';
+      newStr += '!';
     } else{
-      newStr = newStr + str[i];
+      newStr += str[i];
     }
-  } return newStr
+  } return newStr;
 }
 
 function reverse(str) {
   let reverseString = '';
   for (let i=1; i<=str.length; i++){
-    reverseString = reverseString + str[str.length - i]
+    reverseString += str[str.length - i]
   } return reverseString;
 }
 
   function crazyCase2ReturnOfCrazyCase(str) {
-    let newNewStr = '';
-    let lowerCaseString = str.toLowerCase()
+    let newStr = '';
     let counter = 0;
-    for (let i = 0; i<lowerCaseString.length; i++ ){
-      if(lowerCaseString[i] === ' '){
-      newNewStr = newNewStr + ' ';
-      counter++
+    for (let i = 0; i<str.length; i++ ){
+      if(str[i] === ' '){
+      newStr += ' ';
+      counter++;
       } else if (counter%2===0){
-      newNewStr = newNewStr + lowerCaseString[i];
+      newStr += str[i].toLowerCase();
       }else{
-      let newLetter2 = lowerCaseString[i];
-      newNewStr = newNewStr + newLetter2.toUpperCase();
+      newStr += str[i].toUpperCase();
     }counter++
-    } return newNewStr
+    } return newStr;
   }
 
   function titleCase(str) {
     let newStr = '';
-    let lowerCaseString = str.toLowerCase();
     for (let i = 0; i<str.length; i++){
       if (i === 0) {
-        newStr = newStr + lowerCaseString[0].toUpperCase();
-      } else if (lowerCaseString[i] === ' '){
-        newStr = newStr + ' '
-        i++
-        let newestLetter = lowerCaseString[i];
-        let newNewLetter2 = newestLetter.toUpperCase();
-        newStr = newStr + newNewLetter2
+        newStr += str[0].toUpperCase();
+      } else if (str[i] === ' '){
+        newStr += ' '
+        i++;
+        newStr += str[i].toUpperCase();
       } else {
-        newStr = newStr + lowerCaseString[i];
+        newStr += str[i].toLowerCase();
       }
     } return newStr;
   }
@@ -80,30 +72,28 @@ function reverse(str) {
 function onlyVowels(str) {
   let newStr = '';
   for (let i = 0; i < str.length; i++){
-  if ('A,E,I,O,U,a,e,i,o,u'.includes(str[i])){
-    newStr = newStr + str[i]
+  if ('AEIOUaeiou'.includes(str[i])){
+    newStr += str[i]
   }
-  }return newStr 
+  }return newStr;
 }
 
 function crazyCase3SonOfCrazyCase(str) {
-    let newNewStr = '';
-    let lowerCaseString = str.toLowerCase()
+    let newStr = '';
     let counter = 0;
-    for (let i = 0; i<lowerCaseString.length; i++ ){
-      if(lowerCaseString[i] === ' '){
-      newNewStr = newNewStr + ' ';
-      counter++
-      } else if('\,,.,!,@,#,$,%,1,2,3,4,5,6,7,8,9,\(,\)'.includes(str[i])){
-        newNewStr = newNewStr + lowerCaseString[i];
-        counter++
+    for (let i = 0; i<str.length; i++ ){
+      if(str[i] === ' '){
+      newStr += ' ';
+      counter++;
+      } else if('!@#$%123456789,.())'.includes(str[i])){
+        newStr += str[i].toLowerCase();
+        counter++;
       }else if (counter%2===0){
-      newNewStr = newNewStr + lowerCaseString[i];
+        newStr += str[i].toLowerCase();
       }else{
-      let newLetter2 = lowerCaseString[i];
-      newNewStr = newNewStr + newLetter2.toUpperCase();
-    }counter++
-  } return newNewStr
+      newStr += str[i].toUpperCase();
+    }counter++;
+  } return newStr;
 }
 
 
